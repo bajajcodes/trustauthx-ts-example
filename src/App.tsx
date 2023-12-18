bd58a9f675b4195681f421a2b20b5dab292eec82
shubham bajaj <shmbajaj32@gmail.com>
2023-12-18 21:40:01 +0530

    using authclient

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
+import { AuthLiteClient } from 'trustauthx';
+import * as config from './utils/config';
+
 function App() {
+  const authClient = new AuthLiteClient(
+    config.API_KEY,
+    config.API_SECRET,
+    config.ORG_ID
+  );
   return (
     <main>
       <h1>One-Click-Auth/TS-Examples</h1>

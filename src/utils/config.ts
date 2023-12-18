b22b55d550ffe88a0949ed130636bacb13741f28
shubham bajaj <shmbajaj32@gmail.com>
2023-12-18 21:39:51 +0530

    exported config into it's own module

---
+++ src/utils/config.ts
@@ -0,0 +1,5 @@
+const API_KEY: string = import.meta.env.VITE_API_KEY;
+const API_SECRET: string = import.meta.env.VITE_API_SECRET;
+const ORG_ID: string = import.meta.env.VITE_ORG_ID;
+
+export { API_KEY, API_SECRET, ORG_ID };

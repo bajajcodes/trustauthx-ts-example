6cbf1781c6aab0351cf7a07dd9bbfb31a88fbd38
shubham bajaj <shmbajaj32@gmail.com>
2023-12-20 01:03:41 +0530

    code test

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { AuthLiteClient } from 'trustauthx';
+import { AuthLiteClient } from 'trustauthx-shmbajaj';
 import * as config from './utils/config';
 
 function App() {
+  console.log({ config });
   const authClient = new AuthLiteClient(
     config.API_KEY,
     config.API_SECRET,
     config.ORG_ID
   );
+  console.log({ authClient });
+  const url = authClient.generateUrl();
+  console.log({ url });
   return (
     <main>
       <h1>One-Click-Auth/TS-Examples</h1>
